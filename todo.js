6bf8a170feef3fe2c8f6b7c66de07206a754b45d
Hyuk <hyukho83@gmail.com>
2024-12-27 10:43:26 +0900

    update editTodo

---
+++ todo.js
@@ -0,0 +1,32 @@
+// ----- 프로젝트 초기화 -----
+// 초기화 함수: 페이지 로드 시 실행
+function initialize() {}
+
+// ----- 할 일 추가 기능 -----
+// 새로운 할 일을 추가하는 함수
+function addTodo(todo) {}
+
+// ----- 할 일 표시 기능 -----
+// 저장된 할 일 목록을 화면에 표시하는 함수
+function displayTodos(todos) {}
+
+// ----- 할 일 삭제 기능 -----
+// 특정 할 일을 삭제하는 함수
+function deleteTodo(index) {}
+
+// ----- 할 일 수정 기능 -----
+// 특정 할 일을 수정하는 함수
+function editTodo(index, newText) {
+  console.log('editTodo', index, newText);
+}
+
+// ----- 체크박스 상태 변경 기능 -----
+// 할 일 완료/미완료 상태를 토글하는 함수
+function toggleTodoCompletion(index, isChecked) {}
+
+// ----- LocalStorage 연동 -----
+// LocalStorage에서 저장된 할 일 가져오기
+function loadTodos() {}
+
+// LocalStorage에 현재 할 일 저장하기
+function saveTodos(todos) {}
